28d3ff143e575594d51fd4e9993d3ee23239382f
2damb <2damb@PC234006.informatica.edu>
2023-11-16 19:52:40 +0000

    Merge origin/master
    Conflicts:
    	.idea/compiler.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.mycompany.trucable {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
-    requires jakarta.persistence;
 
     opens com.mycompany.trucable.vistas to javafx.fxml;
     exports com.mycompany.trucable;
