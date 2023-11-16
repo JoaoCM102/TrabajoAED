bf370fb5b0f9576e6bffac7d4b082cd73ee9497f
2damb <2damb@PC234006.informatica.edu>
2023-11-16 19:54:17 +0000

    Merge origin/master
    Conflicts:
    	.idea/compiler.xml

--- src/main/java/com/mycompany/trucable/modelo/Usuario.java
+++ src/main/java/com/mycompany/trucable/modelo/Usuario.java
@@ -8,20 +8,20 @@ package com.mycompany.trucable.modelo;
  *
  * @author 2damb
  */
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
-@Entity
-public class Usuario {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String usuario;
-    private String correo;
-    private String nombre;
-    private String clave;
-    
-    
-}
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//
+//@Entity
+//public class Usuario {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Integer id;
+//    private String usuario;
+//    private String correo;
+//    private String nombre;
+//    private String clave;
+//    
+//    
+//}
