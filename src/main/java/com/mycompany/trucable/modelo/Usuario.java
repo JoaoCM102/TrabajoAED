6cda1455cada02b0b7a8e9c8016bec7e583035d7
Erick <elio0eri@gmail.com>
2023-11-16 19:48:56 +0000

    creacion Usuario

---
+++ src/main/java/com/mycompany/trucable/modelo/Usuario.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trucable.modelo;
+
+/**
+ *
+ * @author 2damb
+ */
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String usuario;
+    private String correo;
+    private String nombre;
+    private String clave;
+    
+    
+}
