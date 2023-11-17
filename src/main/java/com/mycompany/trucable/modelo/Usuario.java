6438ac09cfac555093e2975411c9d02ff1541390
Erick <elio0eri@gmail.com>
2023-11-17 18:19:11 +0000

    creando usuario error en persistencia

--- src/main/java/com/mycompany/trucable/modelo/Usuario.java
+++ src/main/java/com/mycompany/trucable/modelo/Usuario.java
@@ -1,27 +1,76 @@
+package com.mycompany.trucable.modelo;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.trucable.modelo;
+
 
 /**
  *
  * @author 2damb
  */
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//
-//@Entity
-//public class Usuario {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//    private String usuario;
-//    private String correo;
-//    private String nombre;
-//    private String clave;
-//    
-//    
-//}
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.io.Serializable;
+
+@Entity
+public class Usuario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String usuario;
+    private String correo;
+    private String nombre;
+    private String clave;
+    
+    public Usuario(Integer id, String usuario, String correo, String nombre, String clave) {
+        this.id = id;
+        this.usuario = usuario;
+        this.correo = correo;
+        this.nombre = nombre;
+        this.clave = clave;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.mycompany.trucable {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires jakarta.persistence;
 
     opens com.mycompany.trucable.vistas to javafx.fxml;
     exports com.mycompany.trucable;
